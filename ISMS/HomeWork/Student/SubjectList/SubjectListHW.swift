cb6f6cc4fb5c41d3e3a7aacdcdc45c08f715ddb4
Atinder Kaur <kauratinderjit@seasiainfotech.com>
2020-04-26 13:14:10 +0530

    atinder

--- ISMS/HomeWork/Student/SubjectList/SubjectListHW.swift
+++ ISMS/HomeWork/Student/SubjectList/SubjectListHW.swift
@@ -40,15 +40,12 @@ class SubjectListHW: BaseUIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         
-   
-        if checkInternetConnection(){
+        if checkInternetConnection() {
             self.ViewModel?.subjectList(studentId : UserDefaultExtensionModel.shared.userRoleParticularId)
-        }else{
+        }else {
             self.showAlert(Message: Alerts.kNoInternetConnection)
         }
-        
     }
-
 
 }
 
